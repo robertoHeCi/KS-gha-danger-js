f7dd0cc22b4608d1610e8061d6730e90b7cfbe16
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-23 22:40:09 +0100

    feat: add pr assignees check

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,2 +1,8 @@
-import { checkPRAssigned } from './src/utils/dangerJs'
-checkPRAssigned()
+// import { checkPRAssigned } from './src/utils/dangerJs'
+// checkPRAssigned()
+
+import { danger, fail } from 'danger'
+
+if (danger.github.pr.assignees.length === 0) {
+  fail('PR must be assigned to somebody')
+}
