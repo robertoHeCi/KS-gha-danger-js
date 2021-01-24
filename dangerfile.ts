a969bde3dbd4f9c50270698802e411e77e0a9d35
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-24 14:07:11 +0100

    fix: revert local approach

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,4 @@
-// import { checkPRAssigned, checkChangedFiles } from './src/utils/dangerJs'
-// import { danger, fail, warn } from 'danger'
-// checkPRAssigned(danger, fail)
-// checkChangedFiles(danger, warn, 1)
-
-import wrapperTs from 'danger-plugin-wrapper-ts'
-
-wrapperTs()
+import { checkPRAssigned, checkChangedFiles } from './src/utils/dangerJs'
+import { danger, fail, warn } from 'danger'
+checkPRAssigned(danger, fail)
+checkChangedFiles(danger, warn, 1)
