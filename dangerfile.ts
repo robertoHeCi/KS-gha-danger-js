0b7293e1c9bece2ca574e624d113808e6859d3d9
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-24 14:02:36 +0100

    test: poc using an external danger wrapper

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,8 @@
-import { checkPRAssigned, checkChangedFiles } from './src/utils/dangerJs'
-import { danger, fail, warn } from 'danger'
-checkPRAssigned(danger, fail)
-checkChangedFiles(danger, warn, 1)
+// import { checkPRAssigned, checkChangedFiles } from './src/utils/dangerJs'
+// import { danger, fail, warn } from 'danger'
+// checkPRAssigned(danger, fail)
+// checkChangedFiles(danger, warn, 1)
+
+import wrapperTs from 'danger-plugin-wrapper-ts'
+
+wrapperTs()
