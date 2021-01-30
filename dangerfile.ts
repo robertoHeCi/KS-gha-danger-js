68b077ada67c2ad16f0fa944e31a6cd89796ba13
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-30 23:36:35 +0100

    feat: add function to danger flow

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,6 @@
-import { checkPRAssigned, checkChangedFiles } from './src/utils/dangerJs'
+import { checkPRAssigned, checkChangedFiles, checkTicketLinkInPrBoby } from './src/utils/dangerJs'
 import { danger, fail, warn } from 'danger'
 // @ts-ignore
 checkPRAssigned(danger, fail)
 checkChangedFiles(danger, warn, 1)
+checkTicketLinkInPrBoby(danger, fail)
