769af35f83009849a41266de899b39ec0125389c
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-30 22:48:02 +0100

    feat: delete unused plugin

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,5 @@
 import { checkPRAssigned, checkChangedFiles } from './src/utils/dangerJs'
-import { danger, fail, warn, schedule } from 'danger'
-import noConsole from 'danger-plugin-no-console'
+import { danger, fail, warn } from 'danger'
 // @ts-ignore
-schedule(noConsole())
 checkPRAssigned(danger, fail)
 checkChangedFiles(danger, warn, 1)
