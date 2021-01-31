0fcc5b92de46e152fc93b6de562ad61034dd36e7
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-31 16:14:05 +0100

    refactor: add type in property

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,7 @@
 import { checkPRReviewers, checkNewDependencies, checkChangedFiles, checkTicketLinkInPrBoby } from './src/utils/dangerJs'
 import { danger, fail, warn, message } from 'danger'
-const dangerJS = { danger, fail, warn, message }
+import { DangerModel } from './src/utils/dangerJs/DangerModel'
+const dangerJS:DangerModel = { danger, fail, warn, message }
 checkPRReviewers(dangerJS)
 checkNewDependencies(dangerJS)
 checkChangedFiles(dangerJS, 1)
