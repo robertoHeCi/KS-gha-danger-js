8e873d99fbb06c46fb91b84d15d54b91275faa21
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-31 16:35:16 +0100

    chore: plugin poc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 
+// FIXME: example of fixme
 function App ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
