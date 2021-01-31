c98d8100c3f8f073003e506f084e9b6db895d284
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-31 16:26:37 +0100

    chore: plugin poc

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,11 @@
 import { checkPRReviewers, checkNewDependencies, checkChangedFiles, checkTicketLinkInPrBoby } from './src/utils/dangerJs'
 import { danger, fail, warn, message } from 'danger'
 import { DangerModel } from './src/utils/dangerJs/DangerModel'
+import fixme from 'danger-plugin-fixme'
 const dangerJS:DangerModel = { danger, fail, warn, message }
+
 checkPRReviewers(dangerJS)
 checkNewDependencies(dangerJS)
 checkChangedFiles(dangerJS, 1)
 checkTicketLinkInPrBoby(dangerJS)
+fixme()
