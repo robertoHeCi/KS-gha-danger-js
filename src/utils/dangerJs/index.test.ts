8bdd0b5dbe50867f4e041fb55750b33067f31da5
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-31 13:35:32 +0100

    refactor: apply builders and modify index function types

--- src/utils/dangerJs/index.test.ts
+++ src/utils/dangerJs/index.test.ts
@@ -1,66 +1,79 @@
 import * as danger from 'danger'
-import { checkChangedFiles, checkNewDependencies, checkPRAssigned, checkTicketLinkInPrBoby } from './index'
+import {
+  checkChangedFiles,
+  checkNewDependencies,
+  checkPRReviewers,
+  checkTicketLinkInPrBoby
+} from './index'
+import { DangerMock, LogFunction } from './DangerModel'
 import { gitHubMockBuilder, gitMockBuilder } from './mocks'
-import { DangerModel } from './DangerModel'
 jest.mock('danger', () => jest.fn())
-const dangerMock:DangerModel = danger
+const dangerMock: { danger: DangerMock, fail: LogFunction, warn: LogFunction, message: LogFunction } = danger
 
 describe('Danger JS tests', () => {
   beforeEach(() => {
     dangerMock.fail = jest.fn()
     dangerMock.warn = jest.fn()
+    dangerMock.danger = danger
   })
 
   it('fails if there are more changed files than 1', () => {
-    dangerMock.danger = { git: { created_files: ['example.ts'], modified_files: ['example.ts'], deleted_files: ['example.ts'], commits: [] } }
+    dangerMock.danger.git = gitMockBuilder(['example.ts'], ['example.ts'], ['example.ts'])
     return checkChangedFiles(dangerMock.danger, dangerMock.warn, 1).then(() => {
       expect(dangerMock.warn).toHaveBeenCalled()
     })
   })
 
   it('Should not fails if there are less changed files than 4', () => {
-    dangerMock.danger = { git: { created_files: ['example.ts'], modified_files: ['example.ts'], deleted_files: ['example.ts'] } }
+    dangerMock.danger.git = gitMockBuilder(['example.ts'], ['example.ts'], ['example.ts'])
     return checkChangedFiles(dangerMock.danger, dangerMock.warn, 4).then(() => {
       expect(dangerMock.warn).not.toHaveBeenCalled()
     })
   })
 
   it('fails if there are no asignees on the PR', () => {
-    dangerMock.danger = { github: { pr: { assignees: [] } } }
-    return checkPRAssigned(dangerMock.danger, dangerMock.fail).then(() => {
+    dangerMock.danger.github = gitHubMockBuilder()
+    return checkPRReviewers(dangerMock.danger, dangerMock.fail).then(() => {
       expect(dangerMock.fail).toHaveBeenCalled()
     })
   })
 
   it('Not fails there are no asignees on the PR', () => {
-    dangerMock.danger = { github: { pr: { assignees: [{ id: 1, login: '', type: 'User', avatar_url: '', href: '' }] } } }
-    return checkPRAssigned(dangerMock.danger, dangerMock.fail).then(() => {
+    dangerMock.danger.github = gitHubMockBuilder('', [{
+      id: 1,
+      login: 'string',
+      type: 'User',
+      avatar_url: 'string',
+      href: 'string'
+    }])
+    return checkPRReviewers(dangerMock.danger, dangerMock.fail).then(() => {
       expect(dangerMock.fail).not.toHaveBeenCalled()
     })
   })
 
   it('Should return a fail because the PR body does not contains the issue link', () => {
-    dangerMock.danger = { github: { pr: { body: '' } } }
+    dangerMock.danger.github = gitHubMockBuilder('')
     return checkTicketLinkInPrBoby(dangerMock.danger, dangerMock.fail).then(() => {
       expect(dangerMock.fail).toHaveBeenCalled()
     })
   })
 
   it('Should not fail because the PR body contains the issue link', () => {
-    dangerMock.danger = { github: { pr: { body: '[AB#12234]()' } } }
+    dangerMock.danger.github = gitHubMockBuilder('[AB#12234]()')
+
     return checkTicketLinkInPrBoby(dangerMock.danger, dangerMock.fail).then(() => {
       expect(dangerMock.fail).not.toHaveBeenCalled()
     })
   })
 
   it('Should execute the log function if modified files contains package.json', () => {
-    dangerMock.danger = { git: { modified_files: ['package.json'] } }
+    dangerMock.danger.git = gitMockBuilder(['package.json'])
     return checkNewDependencies(dangerMock.danger, dangerMock.warn).then(() => {
       expect(dangerMock.warn).toHaveBeenCalled()
     })
   })
   it('Should  not execute the log function if modified files not contains package.json', () => {
-    dangerMock.danger = { git: { modified_files: ['index.json'] } }
+    dangerMock.danger.git = gitMockBuilder(['index.ts'])
     return checkNewDependencies(dangerMock.danger, dangerMock.warn).then(() => {
       expect(dangerMock.warn).not.toHaveBeenCalled()
     })
--- src/utils/dangerJs/index.ts
+++ src/utils/dangerJs/index.ts
@@ -1,11 +1,12 @@
-import { DangerObjectModel, LogFunction } from './DangerModel'
-export const checkPRAssigned = async (danger:DangerObjectModel, fail:LogFunction) => {
-  if (danger?.github?.pr?.assignees.length === 0) {
-    fail('PR must be assigned to somebody!! 🙏')
+import { DangerModel, LogFunction } from './DangerModel'
+
+export const checkPRReviewers = async (danger:DangerModel, fail:LogFunction) => {
+  if (danger?.github?.requested_reviewers?.users.length === 0) {
+    fail('PR must have at least 1 reviewer!!')
   }
 }
 
-export const checkChangedFiles = async (danger:DangerObjectModel, warn:LogFunction, limit = 10) => {
+export const checkChangedFiles = async (danger:DangerModel, warn:LogFunction, limit = 10) => {
   const addedFilesAmount = danger?.git?.created_files?.length || 0
   const changedFilesAmount = danger?.git?.modified_files?.length || 0
   const deletedFilesAmount = danger?.git?.deleted_files?.length || 0
@@ -15,22 +16,22 @@ export const checkChangedFiles = async (danger:DangerObjectModel, warn:LogFuncti
   }
 }
 
-export const checkTicketLinkInPrBoby = async (danger:DangerObjectModel, fail:LogFunction) => {
+export const checkTicketLinkInPrBoby = async (danger:DangerModel, fail:LogFunction) => {
   const ticketRegExp = /AB#[0-9]{5}/g
   if (!danger?.github?.pr?.body.match(ticketRegExp)) {
     fail('Add the ticket for this PR at the PR body')
   }
 }
 
-export const checkNewDependencies = async (danger: DangerObjectModel, warn:LogFunction) => {
+export const checkNewDependencies = async (danger: DangerModel, warn:LogFunction) => {
   if (danger?.git?.modified_files?.includes('package.json')) {
     warn('This PR contains new/updated dependencies. Remember execute npm i before testing the PR!')
   }
 }
 
-export const checkUpdatedTests = (danger: DangerObjectModel, warn:LogFunction, testFilePattern = 'test') => {
-  const addedFiles = danger.git.created_files || []
-  const changedFiles = danger.git.modified_files || []
+export const checkUpdatedTests = (danger: DangerModel, warn:LogFunction, testFilePattern = 'test') => {
+  const addedFiles = danger?.git?.created_files || []
+  const changedFiles = danger?.git?.modified_files || []
   const testFilesUpdated = [...addedFiles, ...changedFiles].filter(filepath =>
     filepath.includes(testFilePattern)
   )
