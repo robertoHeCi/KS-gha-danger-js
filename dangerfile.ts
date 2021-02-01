229a81346d2adc5eae6b7a03cb95895fba54fb18
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-02-01 21:14:49 +0100

    feat: add custom plugin

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,11 +2,11 @@ import { checkPRReviewers, checkNewDependencies, checkChangedFiles, checkTicketL
 import { danger, fail, warn, message } from 'danger'
 import { DangerModel } from './src/utils/dangerJs/DangerModel'
 // @ts-ignore
-import fixme from 'danger-plugin-fixme'
-const dangerJS:DangerModel = { danger, fail, warn, message }
+import wrapperTs from 'danger-plugin-wrapper-ts'
 
+const dangerJS:DangerModel = { danger, fail, warn, message }
 checkPRReviewers(dangerJS)
 checkNewDependencies(dangerJS)
 checkChangedFiles(dangerJS, 1)
 checkTicketLinkInPrBoby(dangerJS)
-fixme(['TODO'])
+wrapperTs()
