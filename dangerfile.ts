404dade316ff18a40a882ef6ccf476bdf1c645cf
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-02-03 09:07:31 +0100

    feat: execute wrapper plugin

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,12 +1,2 @@
-import { checkPRReviewers, checkNewDependencies, checkChangedFiles, checkTicketLinkInPrBoby } from './src/utils/dangerJs'
-import { danger, fail, warn, message } from 'danger'
-import { DangerModel } from './src/utils/dangerJs/DangerModel'
-// @ts-ignore
-import fixme from 'danger-plugin-fixme'
-const dangerJS:DangerModel = { danger, fail, warn, message }
-
-checkPRReviewers(dangerJS)
-checkNewDependencies(dangerJS)
-checkChangedFiles(dangerJS, 1)
-checkTicketLinkInPrBoby(dangerJS)
-fixme(['TODO'])
+import wrapperTs from 'danger-plugin-wrapper-ts'
+wrapperTs()
