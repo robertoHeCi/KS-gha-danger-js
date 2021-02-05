a05a4402ee6f89666527a45641286f4c64d7bdf1
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-02-05 09:02:53 +0100

    refactor: remove danger dependencie in functions

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,5 @@
 import { checkPRReviewers, checkNewDependencies, checkChangedFiles, checkTicketLinkInPrBoby } from './src/utils/dangerJs'
-import { danger, fail, warn, message } from 'danger'
-import { DangerModel } from './src/utils/dangerJs/DangerModel'
-const dangerJS:DangerModel = { danger, fail, warn, message }
-checkPRReviewers(dangerJS)
-checkNewDependencies(dangerJS)
-checkChangedFiles(dangerJS, 1)
-checkTicketLinkInPrBoby(dangerJS)
+checkPRReviewers()
+checkNewDependencies()
+checkChangedFiles(1)
+checkTicketLinkInPrBoby()
