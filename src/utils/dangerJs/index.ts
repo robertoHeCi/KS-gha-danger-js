faafa452df6b9bc3e570ba09cc9d7b0b7675fe6e
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-02-05 09:11:36 +0100

    fix: remove optional characters

--- src/utils/dangerJs/index.ts
+++ src/utils/dangerJs/index.ts
@@ -13,9 +13,9 @@ export const checkPRReviewers = function () {
 }
 
 export const checkChangedFiles = (limit = 10) => {
-  const addedFilesAmount = danger.git.created_files?.length || 0
-  const changedFilesAmount = danger.git.modified_files?.length || 0
-  const deletedFilesAmount = danger.git.deleted_files?.length || 0
+  const addedFilesAmount = danger.git.created_files.length || 0
+  const changedFilesAmount = danger.git.modified_files.length || 0
+  const deletedFilesAmount = danger.git.deleted_files.length || 0
   const filesChanged = addedFilesAmount + changedFilesAmount + deletedFilesAmount
   if (filesChanged > limit) {
     warn(`Files changed in this PR are ${filesChanged}!!. Limit is ${limit} ;)`)
@@ -24,13 +24,13 @@ export const checkChangedFiles = (limit = 10) => {
 
 export const checkTicketLinkInPrBoby = () => {
   const ticketRegExp = /AB#[0-9]{5}/g
-  if (!danger.github?.pr?.body.match(ticketRegExp)) {
+  if (!danger.github.pr.body.match(ticketRegExp)) {
     fail('Add the ticket for this PR at the PR body')
   }
 }
 
 export const checkNewDependencies = () => {
-  if (danger.git.modified_files?.includes('package.json')) {
+  if (danger.git.modified_files.includes('package.json')) {
     warn('This PR contains new/updated dependencies. Remember execute npm i before testing the PR!')
   }
 }
